d12360023f0d38a01a8ce52bf3bbac3563e3696f
a23pablooc <a23pablooc@iessanclemente.net>
2023-10-03 16:27:14 +0200

    LC541: Accepted

--- LeetCode/Java/LC541/src/App.java
+++ LeetCode/Java/LC541/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "abcdefg";
+        int k = 2;
+
+        System.out.println(solution.reverseStr(s, k));
     }
 }
