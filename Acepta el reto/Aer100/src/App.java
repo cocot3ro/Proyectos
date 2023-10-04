646b949c2b34f55507039039fbfce27ab7c7922d
a23pablooc <a23pablooc@iessanclemente.net>
2023-10-04 15:42:21 +0200

    Aer100: Accepted

--- Acepta el reto/Aer100/src/App.java
+++ Acepta el reto/Aer100/src/App.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -6,36 +5,41 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        outer:
         for (int i = sc.nextInt(); i > 0; i--) {
-            String num = String.format("%04d", sc.nextInt());
+            int number = sc.nextInt();
+            String num = String.format("%04d", number);
             char[] chars = num.toCharArray();
             Arrays.sort(chars);
             num = new String(chars);
 
-            BigInteger b1 = new BigInteger(new StringBuilder(num).reverse().toString());
-            BigInteger b2 = new BigInteger(num);
+            int b1 = Integer.parseInt(new StringBuilder(num).reverse().toString());
+            int b2 = Integer.parseInt(num);
 
-            BigInteger b3;
-            int ans = 0;
-            while (!(b3 = b1.subtract(b2)).equals(new BigInteger("6174"))) {
-                if (b3.equals(new BigInteger("0"))) {
-                    System.out.println(8);
-                    continue outer;
-                }
+            if (number == 6174) {
+                System.out.println(0);
+                continue;
+            }
+
+            if (b1 == b2) {
+                System.out.println(8);
+                continue;
+            }
+
+            int b3;
+            int ans = 1;
+            while ((b3 = b1 - b2) != 6174) {
                 ans++;
 
-                num = String.format("%04d", Integer.parseInt(b3.toString()));
+                num = String.format("%04d", b3);
                 chars = num.toCharArray();
                 Arrays.sort(chars);
                 num = new String(chars);
 
-                b1 = new BigInteger(new StringBuilder(num).reverse().toString());
-                b2 = new BigInteger(num);
+                b1 = Integer.parseInt(new StringBuilder(num).reverse().toString());
+                b2 = Integer.parseInt(num);
             }
-            System.out.println(ans != 0 ? ans + 1 : ans);
-        }
+            System.out.println(ans);
 
-        sc.close();
+        }
     }
 }
