736911827f3c4cb02e9ebf792b7b42bb159d971e
a23pablooc <a23pablooc@iessanclemente.net>
2023-10-30 11:33:20 +0100

    LC1356: Accepted

--- LeetCode/Java/LC1356/src/Solution.java
+++ LeetCode/Java/LC1356/src/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
         Map<Integer, Map<Integer, Integer>> map = new TreeMap<>();
 
         for (int i : arr) {
-            int count = countBits(i);
+            int count = Integer.bitCount(i);
             Map<Integer, Integer> subMap = map.get(count);
             if (subMap == null) {
                 map.put(count, new TreeMap<>(Map.of(i, 1)));
@@ -14,14 +14,14 @@ public class Solution {
             }
         }
 
+        int idx = 0;
         for (Map.Entry<Integer, Map<Integer, Integer>> subMap : map.entrySet()) {
-
+            for (Map.Entry<Integer, Integer> subSubMap : subMap.getValue().entrySet()) {
+                for (int i = 0; i < subSubMap.getValue(); i++) {
+                    arr[idx++] = subSubMap.getKey();
+                }
+            }
         }
-
         return arr;
-    }
-
-    private int countBits(int val) {
-        return String.valueOf(val).replace("0", "").length();
     }
 }
