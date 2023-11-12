9d80686e9b10b5d2ca9cbb02bde9aa6c3fdc2a43
cocot3ro <pablooterocastro15@gmail.com>
2023-11-12 16:52:42 +0100

    LC2642: Wrong Answer

--- LeetCode/Java/LC2642/src/Graph.java
+++ LeetCode/Java/LC2642/src/Graph.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Graph {
-    private static class Node {
+
+    class Node {
         int val;
         boolean explored;
         Map<Node, Integer> next;
@@ -17,15 +18,24 @@ public class Graph {
             this.minorTime = Integer.MAX_VALUE;
         }
 
-        void addNext(int n, int length) {
-            next.put(Singleton.getInstance(n), length);
+        void addNext(int n, int time) {
+            next.put(singleton.getInstance(n), time);
+        }
+
+        void reset() {
+            explored = false;
+            minorTime = Integer.MAX_VALUE;
         }
     }
 
-    private static class Singleton {
-        static Map<Integer, Node> singleton = new HashMap<>();
+    class Singleton {
+        Map<Integer, Node> singleton;
 
-        static Node getInstance(int n) {
+        Singleton() {
+            this.singleton = new HashMap<>();
+        }
+
+        Node getInstance(int n) {
             if (!singleton.containsKey(n)) {
                 singleton.put(n, new Node(n));
             }
@@ -34,22 +44,64 @@ public class Graph {
 
     }
 
-    private List<Node> graph;
+    private final Singleton singleton;
+    boolean unreachable;
+    private final List<Node> list;
 
     public Graph(int n, int[][] edges) {
-        this.graph = new LinkedList<>();
+        this.singleton = new Singleton();
+        list = new LinkedList<>();
         for (int[] edge : edges) {
             addEdge(edge);
         }
     }
 
     public void addEdge(int[] edge) {
-        Node node = Singleton.getInstance(edge[0]);
+        Node node = singleton.getInstance(edge[0]);
         node.addNext(edge[1], edge[2]);
-        graph.add(node);
+        list.add(node);
     }
 
     public int shortestPath(int node1, int node2) {
-        return -1;
+        this.unreachable = false;
+        list.forEach(Node::reset);
+        singleton.getInstance(node1).minorTime = 0;
+
+        int ans = recursive(node1, node2);
+        System.out.println("ans = " + ans);
+        return (unreachable ? -1 : ans);
+    }
+
+    private int recursive(int node1, int node2) {
+        if (node1 == node2) {
+            return 0;
+        }
+
+        if (unreachable) {
+            return -1;
+        }
+
+        Node thisNode = singleton.getInstance(node1);
+        thisNode.explored = true;
+        int minorTime = Integer.MAX_VALUE;
+        Node minorNode = null;
+        for (Map.Entry<Node, Integer> entry : thisNode.next.entrySet()) {
+            Node aNextNode = entry.getKey();
+            if (!aNextNode.explored) {
+                int newTime = thisNode.minorTime + entry.getValue();
+                if (newTime < aNextNode.minorTime) {
+                    aNextNode.minorTime = newTime;
+                    minorTime = aNextNode.minorTime;
+                    minorNode = aNextNode;
+                }
+            }
+        }
+
+        if (minorNode == null) {
+            this.unreachable = true;
+            return -1;
+        }
+
+        return minorTime + recursive(minorNode.val, node2);
     }
 }
