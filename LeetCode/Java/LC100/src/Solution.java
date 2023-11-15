0bcd8b368d4eff74704f6a135d133064f7d5fb55
dam2 - Otero Castro <A23PabloOC@sanclemente.local>
2023-11-15 16:41:04 +0100

    Optimizado con StringBuilder

--- LeetCode/Java/LC100/src/Solution.java
+++ LeetCode/Java/LC100/src/Solution.java
@@ -1,19 +1,19 @@
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        String pTree = toString(p);
-        String qTree = toString(q);
+        String pTree = toString(p).toString();
+        String qTree = toString(q).toString();
         return pTree.equals(qTree);
     }
 
-    private String toString(TreeNode node) {
+    private StringBuilder toString(TreeNode node) {
         if (node == null) {
-            return "";
+            return new StringBuilder();
         }
 
-        String str = "";
-        str += toString(node.left) + ";";
-        str += node.val + ";";
-        str += toString(node.right) + ";";
+        StringBuilder str = new StringBuilder();
+        str.append(toString(node.left)).append(";");
+        str.append(node.val).append(";");
+        str.append(toString(node.right)).append(";");
 
         return str;
     }
