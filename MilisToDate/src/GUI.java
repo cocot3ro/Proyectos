5e8f55064dd2e3f4146450f61c9cf64843cd9fcc
a23pablooc <a23pablooc@iessanclemente.net>
2023-11-30 13:26:48 +0100

    Simple converter with GUI

---
+++ MilisToDate/src/GUI.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GUI extends JFrame {
+
+    private final JTextField txt1;
+    private final JTextField txt2;
+    private final JTextField txt3;
+    private final JButton btn;
+
+    private GUI() {
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 100);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        txt1 = new JTextField(10);
+        txt2 = new JTextField(10);
+        txt3 = new JTextField(10);
+        btn = new JButton("Convert");
+
+        btn.addActionListener(actionEvent -> {
+            if (txt1.getText().contains(":")) {
+                txt2.setText(convertHour(txt1.getText()) + "");
+                txt3.setText("");
+            } else if (txt1.getText().contains("/")) {
+                txt2.setText(convertDate(txt1.getText()) + "");
+                txt3.setText("");
+            } else {
+                txt2.setText(convertDate(Long.parseLong(txt1.getText())));
+                txt3.setText(convertHour(Long.parseLong(txt1.getText())));
+            }
+        });
+
+        add(txt1);
+        add(txt2);
+        add(txt3);
+        add(btn);
+    }
+
+    public Long convertHour(String hora) {
+        try {
+            SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+            Date fecha = formatoHora.parse(hora);
+            return fecha.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return -1L;
+        }
+    }
+
+    public String convertHour(long milisegundos) {
+        SimpleDateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+        Date fecha = new Date(milisegundos);
+        return formatoHora.format(fecha);
+    }
+
+    public Long convertDate(String fecha) {
+        try {
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+            Date fechaObjeto = formatoFecha.parse(fecha);
+            return fechaObjeto.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return -1L;
+        }
+    }
+
+    // Método para convertir milisegundos a una fecha en formato DD:MM:AAAA
+    public String convertDate(long milisegundos) {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+        Date fecha = new Date(milisegundos);
+        return formatoFecha.format(fecha);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(GUI::new);
+    }
+}
