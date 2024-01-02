f799a18f20fba0fbd9ea8ee8c28e9ec6011bc647
cocot3ro <pablooterocastro15@gmail.com>
2024-01-02 14:38:03 +0100

    LC2610: Accepted

--- LeetCode/Java/LC2610/src/Solution.java
+++ LeetCode/Java/LC2610/src/Solution.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> matrix = new LinkedList<>();
-        matrix.add(new LinkedList<>());
 
         outer:
         for (Integer num : nums) {
