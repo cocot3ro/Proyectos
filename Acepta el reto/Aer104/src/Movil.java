760b9319ead9afb50236d8167df9c84a59f39474
cocot3ro <pablooterocastro15@gmail.com>
2024-03-25 23:44:20 +0100

    Movidos todos los ejercicios de Acepta El Reto para un repositorio exclusivo para ello

--- Acepta el reto/Aer104/src/Movil.java
+++
@@ -1,6 +0,0 @@
-public abstract class Movil {
-
-    abstract int getPeso();
-
-    abstract boolean estaBalanceado();
-}
--- Acepta el reto/Aer104/src/Scanner.java
+++
@@ -1,7 +0,0 @@
-public class Scanner {
-    static java.util.Scanner sc = new java.util.Scanner(System.in);
-
-    static int nextInt() {
-        return sc.nextInt();
-    }
-}
