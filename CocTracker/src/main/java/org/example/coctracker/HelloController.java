2d65053fdb98c19f5f509d55875c4a57ba31de8d
a23pablooc <a23pablooc@iessanclemente.net>
2024-04-04 22:59:55 +0200

    nada

--- CocTracker/src/main/java/org/example/coctracker/HelloController.java
+++ CocTracker/src/main/java/org/example/coctracker/HelloController.java
@@ -1,14 +1,8 @@
 package org.example.coctracker;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.*;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
