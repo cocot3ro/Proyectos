9e4c761b10f8586e8e302fc315af2d1604849336
cocot3ro <pablooterocastro15@gmail.com>
2024-04-14 12:32:43 +0200

    nada

--- CocTracker/src/main/java/com/example/CocTracker/StageInitializer.java
+++ CocTracker/src/main/java/com/example/CocTracker/StageInitializer.java
@@ -1,14 +1,27 @@
 package com.example.CocTracker;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
+
     @Override
     public void onApplicationEvent(StageReadyEvent event) {
-        Stage stage = event.getStage();
-        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainFrameApplication.class.getResource("main-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+            Stage stage = event.getStage();
+            stage.setTitle("Clash of Clans Data Tracker");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
